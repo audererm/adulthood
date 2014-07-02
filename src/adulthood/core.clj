6a26f954d1003a274f4cdabcab89b79f5477bd49
aeyk <metalavocado@gmail.com>
2014-07-02 12:33:52 -0400

    refactor ;)

--- src/adulthood/core.clj
+++ src/adulthood/core.clj
@@ -1,12 +1,14 @@
 (ns adulthood.core
   (:gen-class)
-  (:require [clj-time.core :as t]))
+  (:require [clj-time.core :refer [in-days interval date-time]]))
 
-(require '[clj-time.core :as t])
+
+(defn time-til [when]
+  (in-days (interval (now) when)))
 
 (defn -main
   "Print how many days left until I turn 18."
   [& args]
   (println "You have" 
-           (t/in-days (t/interval (t/now) (t/date-time 2015 7 27)))
+           (time-til (date-time 2015 7 27))
            "days remaining until adulthood."))
